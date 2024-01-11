c1d7862c44af89069a669cd0db1e043d4d727bc9
raquel-digital <93403242+raquel-digital@users.noreply.github.com>
2024-01-11 15:52:50 -0300

    busqueda local

--- api/arts/controller.js
+++ api/arts/controller.js
@@ -114,7 +114,9 @@ controller = {
      },
 
     buscarArticulo: async function(query){
-       const result = await store.search(query);
+       //const result = await store.search(query);
+       const buscador = require("../../utils/buscadorLocal")
+       const result = buscador(query)
        return result;
     },
     buscarArticuloPorColor: async function(query){
---
+++ utils/buscadorLocal.js
@@ -0,0 +1,25 @@
+function buscador(query){
+    //limpia cache del modulo precios
+    delete require.cache[require.resolve("../public/system/dir/allArts.json")];
+    //vuelve a cargarlo en caso que se actualice
+    const arts = require("../public/system/dir/allArts.json")
+    
+    let res = arts.filter(e => {
+        if(e.codigo.includes(query.toUpperCase())){
+            return e
+        }
+        if(e.nombre.includes(query.toUpperCase())){
+            return e
+        }
+        if(e.nombre2.includes(query)){
+            return e
+        }
+    })
+    if(res.length == 0){
+        const mongo = require("../api/arts/store")
+        res = mongo.search(query)
+    }
+    return res
+}
+
+module.exports = buscador
